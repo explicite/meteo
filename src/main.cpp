18c52eb12885f7fff2735738444d0fcab0b87b45
Jan Paw <jpaw@virtuslab.com>
2016-03-09 08:54:33 +0100

    travis support

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 
 void setup() {
   pinMode(2, OUTPUT);
-  int WiFiCounter = 0;
   WiFi.disconnect();
   WiFi.mode(WIFI_STA);
   WiFi.begin("ssid", "pass");
+  int WiFiCounter = 0;
   while (WiFi.status() != WL_CONNECTED && WiFiCounter < 30) {
     digitalWrite(2, LOW);
     delay(1000);
@@ -18,8 +18,8 @@ void setup() {
 
 void loop() {
   if(WiFi.status() == WL_CONNECTED){
-    digitalWrite(2, HIGH);
-  } else {
     digitalWrite(2, LOW);
+  } else {
+    digitalWrite(2, HIGH);
   }
 }
