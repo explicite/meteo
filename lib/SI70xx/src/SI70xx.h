eb668558462b565c26092f2383756fb8e5266a5d
Jan Paw <jpaw@virtuslab.com>
2016-03-18 09:39:05 +0100

    fix format

--- lib/SI70xx/src/SI70xx.h
+++ lib/SI70xx/src/SI70xx.h
@@ -8,7 +8,7 @@ class SI70xx {
   public:
     SI70xx(u8 ADDR);
     int init(u8 SDA, u8 SCL);
-    int getDeviceId(u8 *id);
+    int getDeviceId(u8* id);
     int getHumidity();
     int getTemperature();
   private:
@@ -17,9 +17,9 @@ class SI70xx {
     u8 _SDA;
     u8 _SCL;
 
-    int _writeReg(u8 * reg, u8 reglen);
-    int _readReg(u8 * reg, u8 reglen);
-    int _command(u8 cmd, u8 * buf);
+    int _writeReg(u8* reg, u8 reglen);
+    int _readReg(u8* reg, u8 reglen);
+    int _command(u8 cmd, u8* buf);
     int _measure(u8 cmd);
 };
 
