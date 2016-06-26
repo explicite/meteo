9cb505480c048d0fba404451308ba682d4e0fc70
Jan Paw <jpaw@localhost.localdomain>
2016-06-26 21:42:06 +0200

    Additionals in measurement: vcc, id

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,29 @@
 #include <BH17xx.hpp>
 #include <LPS331AP.hpp>
 
+#define BAUD          0xE100
 #define SDA           0x00 // GPIO0
 #define SCL           0x02 // GPIO2
-#define WAKEUP_PIN    0x0C // GRI12
-#define SLEEP_TIME    0x1E
+#define WAKEUP_PIN    0x0C // GPI16
+#define SLEEP_TIME    0x96 // 60 seconds
 #define WIFI_CON_TRY  0x1E
 #define SI7021_ADDR   0x40
 #define BH1750_ADDR   0x23
 #define LPS331AP_ADDR 0x5d
-#define SSID ""
-#define PASSWD ""
+#define SSID "node"
+#define PASSWD "node"
 #define DEBUG
 #undef  DEBUG
 
-IPAddress server(212, 47, 230, 159);
+ADC_MODE(ADC_VCC);
+IPAddress server(000, 000, 000, 000);
 
 SI70xx si7021(SI7021_ADDR);
 BH17xx bh1750(BH1750_ADDR, BH1750);
 LPS331AP lps331ap(LPS331AP_ADDR);
 WiFiClient client;
+String id = String(ESP.getChipId());
+String vcc;
 String temperatureSI7021;
 String humiditySI7021;
 String illuminanceBH1750;
@@ -32,6 +36,7 @@ String pressureLPS331AP;
 String body;
 
 void send() {
+  vcc = String(ESP.getVcc());
   temperatureSI7021 = String(si7021.getTemperature()/1000.0);
   humiditySI7021 = String(si7021.getHumidity()/1000.0);
   illuminanceBH1750 = String(bh1750.getIlluminance());
@@ -39,6 +44,8 @@ void send() {
   pressureLPS331AP = String(lps331ap.getPressure());
 
   body = String(
+    "esp8266,sensor=esp8266,type=id value=" + id + "\n" +
+    "esp8266,sensor=esp8266,type=vcc value=" + vcc + "\n" +
     "esp8266,sensor=si7021,type=humidity value=" + humiditySI7021 + "\n" +
     "esp8266,sensor=si7021,type=temperature value=" + temperatureSI7021 + "\n" +
     "esp8266,sensor=bh1750,type=illuminance value=" + illuminanceBH1750 + "\n" +
@@ -69,8 +76,8 @@ void send() {
 
 void setup() {
   #ifdef DEBUG
-  Serial.begin(115200);
-  Serial.println("WAKE UP");
+  Serial.begin(BAUD);
+  Serial.println("\nWAKE UP");
   #endif
 
   pinMode(WAKEUP_PIN, INPUT_PULLUP);
@@ -89,7 +96,7 @@ void setup() {
 
   send();
 
-  ESP.deepSleep(SLEEP_TIME * 2 * 1000000);
+  ESP.deepSleep(SLEEP_TIME * 1000000);
 }
 
 void loop() {
